bfcf63fbda948625af27eb862020831d3177180d
zkliu611 <zkliu61@gmail.com>
2018-12-08 12:08:01 -0800

    20 test pass

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,9 +3,15 @@ const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
   if (Object.keys(req.cookies).length > 0) {
-    models.Sessions.get(req.cookies)
+    models.Sessions.get({'hash': req.cookies.shortlyid})
       .then((result) => {
-          
+        req.session = result;
+        res.cookie('shortlyid', result.hash);
+        next();
+      })
+      .catch(err => {
+        res.status(500).send(err);
+        res.end();
       });
   } else {
     models.Sessions.create()
@@ -14,8 +20,7 @@ module.exports.createSession = (req, res, next) => {
       })
       .then(hash => {
         req.session = hash;
-        res.cookies['shortlyid'] = {};
-        res.cookies.shortlyid.value = hash.hash;
+        res.cookie('shortlyid', hash.hash);
         next();
       })
       .catch(err => {
