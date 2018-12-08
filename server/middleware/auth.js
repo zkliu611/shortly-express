3e2d3826d0945d3ae2da44bc298b7dd66a2b8645
zkliu611 <zkliu61@gmail.com>
2018-12-08 11:42:07 -0800

    finish no cookies sessions

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,27 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  if (Object.keys(req.cookies).length > 0) {
+    models.Sessions.get(req.cookies)
+      .then((result) => {
+          
+      });
+  } else {
+    models.Sessions.create()
+      .then((result)=> {
+        return models.Sessions.get({'id': result.insertId});
+      })
+      .then(hash => {
+        req.session = hash;
+        res.cookies['shortlyid'] = {};
+        res.cookies.shortlyid.value = hash.hash;
+        next();
+      })
+      .catch(err => {
+        res.status(500).send(err);
+        res.end();
+      });
+  }
 };
 
 /************************************************************/
