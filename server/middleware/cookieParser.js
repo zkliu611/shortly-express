96ca0fb2de5f0db594239f360a8aac09e3c0cc4a
zkliu611 <zkliu61@gmail.com>
2018-12-07 20:09:42 -0800

    passed first cookieparser

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,16 @@
-const parseCookies = (req, res, next) => {
+const parseCookies = (req, res, next) => { 
+  if (req.headers.cookie) {
+    let cookiesArray = req.headers.cookie.split('; ');
+    var cookies = {};
+    for (let i = 0; i < cookiesArray.length; i++) {
+      var array = cookiesArray[i].split('=');
+      cookies[array[0]] = array[1];
+    } 
+    req.cookies = cookies;
+  } else {
+    req.cookies = {};
+  }
+  next();
 };
 
 module.exports = parseCookies;
